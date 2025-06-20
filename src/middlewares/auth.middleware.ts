436cb307efea18bf9b6ed3a2ead3816339bd383d
Avhad-Enterprises <awesomeboysboys@gmail.com>
2025-06-20 19:20:36 +0530

    Node REST API

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -10,8 +10,8 @@ import { IsEmpty } from 'class-validator';
 const authMiddleware: RequestHandler = async (req, res, next) => {
 
   try {
-    //if (req.path.includes('/employee/login') || req.path.includes('/public')) {
-    if (req.path.includes('/users/login')) {
+    if (req.path.includes('/users/login') || req.path.includes('/users/insertemployee')) {
+      //if (req.path.includes('/users/login')) {
       await DB.raw("SET search_path TO public");
       return next();
     }
